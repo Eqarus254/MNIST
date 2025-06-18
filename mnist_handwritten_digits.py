380c91abe3d4d1a809ea6918fd4dd31005916076
Eqarus254 <88176529+Eqarus254@users.noreply.github.com>
2025-06-18 11:07:57 +0300

    Update mnist_handwritten_digits.py

--- mnist_handwritten_digits.py
+++ mnist_handwritten_digits.py
@@ -14,6 +14,7 @@ import tensorflow as tf
 from tensorflow.keras import layers, models
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.utils import to_categorical
+from PIL import Image
 
 # Load and preprocess data
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
